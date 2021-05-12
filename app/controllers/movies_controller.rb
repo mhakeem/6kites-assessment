10031b710d1d50e2bff389bb96e958763f61594b
Mazin Hakeem <mazhak@msn.com>
2021-05-11 23:28:24 -0600

    extracted OMDB API calls to a PORO Movie model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,41 +3,18 @@ class MoviesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def movie
-
     @movie_id = params[:id]
-
-    @movie = {}
-
-    key = ENV['OMDB_API_KEY']
-
-    results = HTTParty.get("http://www.omdbapi.com/?apikey=#{key}&i=#{@movie_id}&type=movie")
-
-    parsed = results.parsed_response
-
-    @movie = parsed
-
+    @movie = Movie.get(@movie_id)
   end
 
   def search
-
     @term = params[:term]
 
-    if (@term)
-
-      key = ENV['OMDB_API_KEY']
-
-      results = HTTParty.get("http://www.omdbapi.com/?apikey=#{key}&s=#{@term}&type=movie")
-
-      parsed = results.parsed_response
-
-      @movies = parsed['Search']
-
+    unless (@term.blank?)
+      @movies = Movie.search(@term)
     else
-
       @movies = []
-
     end
-
   end
 
 end
---
+++ app/models/movie.rb
@@ -0,0 +1,20 @@
+class Movie
+  API_KEY  = ENV['OMDB_API_KEY']
+  BASE_URL = "http://www.omdbapi.com/?apikey=#{API_KEY}&type=movie"
+
+  def initialize
+    
+  end
+
+  def self.get(movie_id)
+    results = HTTParty.get(BASE_URL + "&i=#{movie_id}")
+    parsed = results.parsed_response
+    parsed
+  end
+
+  def self.search(term)
+      results = HTTParty.get(BASE_URL + "&s=#{term}")
+      parsed = results.parsed_response
+      parsed['Search']
+  end
+end
